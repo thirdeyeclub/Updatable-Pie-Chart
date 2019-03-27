39c3ce3182e74a908948fe1492b3f6abfc236dfa
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-26 20:00:51 -0400

    added styled compenets

--- pie-chart-app/src/App.js
+++ pie-chart-app/src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Pie from './Components/Pie';
 import Form from './Components/Form';
+import styled from 'styled-components';
+
+const FluidContainer = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0 auto;
+align-items: center;
+`
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <FluidContainer>
         <Pie />
         <Form />
-      </div>
+      </FluidContainer>
       
     );
   }
