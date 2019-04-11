6e7b7af231c7771cceaccfa0fc30f204bb028486
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-11 19:41:31 -0400

    api get request '/' worte in server.js basic

--- 02_standard-pie/server.js
+++ 02_standard-pie/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const server = express();
 const helmet = require('helmet');
 const cors = require('cors');
-// const popRouter = require('./routes/pop-route');
 
+
+const server = express();
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
+const utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
 
-// server.use('/pop', popRouter);
+server.get('/', (req, res) => {
+res.status(200).send(`Today is ${utc}`);
+});
+
 
 module.exports = server;
 
