54c9765af8b3100c1ba4870e612e21f5394f9c85
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-10 22:30:54 -0400

    reorginzed repo and reloved pointless data folder, regened databas and delted broken routes

--- 02_standard-pie/server.js
+++ 02_standard-pie/server.js
@@ -2,17 +2,14 @@ const express = require('express');
 const server = express();
 const helmet = require('helmet');
 const cors = require('cors');
-const popRouter = require('./routes/pop-route');
+// const popRouter = require('./routes/pop-route');
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send('Hello from Express');
-});
 
-server.use('/pop', popRouter);
+// server.use('/pop', popRouter);
 
 module.exports = server;
 
