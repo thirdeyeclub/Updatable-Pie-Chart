f51164de999c458e92d078c715f2d5da847bece1
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-11 21:49:36 -0400

    wrote s-test yarn scrpt command to test server

---
+++ 02_standard-pie/server.spec.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const server = require('./server.js');
+
+describe('SERVER', () => {
+    //Date
+    it('is live & date sent', () => {
+        return request(server)
+        .get('/')
+        .then(res=>{
+            expect(res.status).toBe(200)        
+        })
+    });
+    //
+    it('', () => {
+        
+    });
+});
